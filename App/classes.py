629e28baf03e67b010e10e384cdbf5c32a8e2f1c
Aarrn33 <76686175+Aarrn33@users.noreply.github.com>
2022-10-16 16:54:52 +0200

    added the fighter class

--- App/classes.py
+++ App/classes.py
@@ -1,11 +1,11 @@
-import Utilities.dice as Dice
+import Utilities.dice as dice
 # A class that is used to create classes
 class Classes:
-    def __init__(self, name: str, hd: Dice.Dice, st: list, skills: list, nbskills: int):
+    def __init__(self, name: str, hd: dice.Dice, st: list, skills: list, nbskills: int):
         self.name = name #N Name of the class
         self.hd = hd # Hit dice
         self.st = st # Skills which have saving throws
         self.skills = skills # Skills that the player can choose to have proficiency in
         self.nbskills = nbskills # Number of those skills that the plyer can choose
 
-fighter = Classes("Fighter", )
+fighter = Classes("Fighter", dice.d10, ["Strength", "Constitution"], ["Acrobatics", "Animal Handling", "Athletics", "History", "Insight", "Intimidation", "Perception"], 2)
