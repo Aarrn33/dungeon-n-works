6114fbb80ab2f1bbd9e2f31423a61e030e51eb63
Aarrn33 <76686175+Aarrn33@users.noreply.github.com>
2022-10-16 18:43:15 +0200

    updated the ability system

--- App/characters.py
+++ App/characters.py
@@ -1,6 +1,7 @@
 import App.races as Races
 import App.classes as Classes
 import App.abilities as Abilities
+import App.skills as Skills
 
 # This class is used to create and manage a DnD character
 class Character:
@@ -19,6 +20,30 @@ class Character:
         self.intelligence = Abilities.Ability("Intelligence") # Reasoning and Memory
         self.constitution = Abilities.Ability("Constitution") # Endurance
         
+        # Calculates skills
+        # Strength dependants
+        self.athletics = Skills.Skill("Athletics")
+        # Dexterity dependants
+        self.acrobatics = Skills.Skill("Acrobatics")
+        self.sleight_of_hand = Skills.Skill("Sleight of Hand")
+        self.stealth = Skills.Skill("Stealth")
+        # Intelligence dependants
+        self.arcana = Skills.Skill("Arcana")
+        self.history = Skills.Skill("History")
+        self.investigation = Skills.Skill("Investigation")
+        self.nature = Skills.Skill("Nature")
+        self.religion = Skills.Skill("Religion")
+        # Wisdom dependants
+        self.animal_handling = Skills.Skill("Animal Handling")
+        self.insight = Skills.Skill("Insight")
+        self.medicine = Skills.Skill("Medicine")
+        self.perception = Skills.Skill("Perception")
+        self.survival = Skills.Skill("Survival")
+        # Charisma dependants
+        self.deception = Skills.Skill("Deception")
+        self.intimidation = Skills.Skill("Intiimidation")
+        self.performance = Skills.Skill("Performance")
+        self.persuasion = Skills.Skill("Persuasion")
         #TODO Incoporate races
         #TODO Incorporate classes
-        #TODO Calculates skills
+        
--- App/skills.py
+++ App/skills.py
@@ -1,28 +1,9 @@
+import App.Abilities as Abilities
+
 # A class that implements the skills deriving from the abilities
 class Skill:
-    def __init__(self, name: str):
+    def __init__(self, name: str, dependancy: Abilities.Ability):
         self.name = name
-
-# Strength dependants
-athletics = Skill("Athletics")
-# Dexterity dependants
-acrobatics = Skill("Acrobatics")
-sleight_of_hand = Skill("Sleight of Hand")
-stealth = Skill("Stealth")
-# Intelligence dependants
-arcana = Skill("Arcana")
-history = Skill("History")
-investigation = Skill("Investigation")
-nature = Skill("Nature")
-religion = Skill("Religion")
-# Wisdom dependants
-animal_handling = Skill("Animal Handling")
-insight = Skill("Insight")
-medicine = Skill("Medicine")
-perception = Skill("Perception")
-survival = Skill("Survival")
-# Charisma dependants
-deception = Skill("Deception")
-intimidation = Skill("Intiimidation")
-performance = Skill("Performance")
-persuasion = Skill("Persuasion")
+        self.dependancy = dependancy
+        self.value = self.dependancy.value
+        self.modifier = self.dependancy.modifier
