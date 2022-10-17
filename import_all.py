a722718bade838c7e20e16cdc47f0a828c2fcbae
Aarrn33 <76686175+Aarrn33@users.noreply.github.com>
2022-10-17 20:08:52 +0200

    added a file to import everything right now

---
+++ import_all.py
@@ -0,0 +1,7 @@
+import App.abilities as Abilities
+import App.skills as Skills
+import App.races as Races
+import App.classes as Classes
+import App.characters as Characers
+import App.Utilities.dice as Dice
+import App.Utilities.score2modifier as Score2Modifier
