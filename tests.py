3b3bb50bae02677f9f9eb2d9cef9e3db71d54f7f
Aarrn33 <76686175+Aarrn33@users.noreply.github.com>
2022-10-17 20:13:45 +0200

    updated tests

--- tests.py
+++ tests.py
@@ -1,9 +1,11 @@
 import App.abilities as Abilities
+from App.characters import Character
 import App.skills as Skills
 import App.races as Races
 import App.classes as Classes
-import App.characters as Characers
+import App.characters as Characters
 import App.Utilities.dice as Dice
 import App.Utilities.score2modifier as Score2Modifier
 
-Legolis
+Legolis = Characters.Character("Legolis", Races.human, Classes.fighter)
+print(Legolis)
