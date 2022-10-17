8cce23b09f5f1cc3113e9af0f27eb4788b518bf6
Aarrn33 <76686175+Aarrn33@users.noreply.github.com>
2022-10-17 21:49:36 +0200

    added empty file for saving throws

---
+++ App/saving_throws.py
@@ -0,0 +1,6 @@
+# A class that implements the saving throws mechanic
+# A saving throw is used in an attemp to resist something (a trap, poison...)
+# A saving throw for dexterity for exemple is equal to
+# d20 + dex modifier + advantage + proficiency + saving throw bonus for dexterity
+
+#TODO: Implement mechanic
