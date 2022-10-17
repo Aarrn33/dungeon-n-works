94cd4a300274294109e1fc258a0362705b6b1f03
Aarrn33 <76686175+Aarrn33@users.noreply.github.com>
2022-10-17 21:01:46 +0200

    fixed bug where most abilities and all skills weren't properly called

--- App/characters.py
+++ App/characters.py
@@ -19,38 +19,37 @@ class Character:
         self.wisdow = Abilities.Wisdom() # Perception and Insight
         self.charisma = Abilities.Charisma() # Personnality strength
         self.dexterity = Abilities.Dexterity() # Agility
-        self.intelligence = Abilities.Intelligence # Reasoning and Memory
-        self.constitution = Abilities.Constitution # Endurance
+        self.intelligence = Abilities.Intelligence() # Reasoning and Memory
+        self.constitution = Abilities.Constitution() # Endurance
         
         # Calculates default armor class
         # AC = 10 + dexterity
         self.ac = 10 + self.dexterity.modifier
-        print(self.ac)
         
         # Calculates skills
         # Strength dependants
-        self.athletics = Skills.Athletics
+        self.athletics = Skills.Athletics()
         # Dexterity dependants
-        self.acrobatics = Skills.Acrobatics
-        self.sleight_of_hand = Skills.Sleight_of_Hand
-        self.stealth = Skills.Stealth
+        self.acrobatics = Skills.Acrobatics()
+        self.sleight_of_hand = Skills.Sleight_of_Hand()
+        self.stealth = Skills.Stealth()
         # Intelligence dependants
-        self.arcana = Skills.Arcana
-        self.history = Skills.History
-        self.investigation = Skills.Investigation
-        self.nature = Skills.Nature
-        self.religion = Skills.Religion
+        self.arcana = Skills.Arcana()
+        self.history = Skills.History()
+        self.investigation = Skills.Investigation()
+        self.nature = Skills.Nature()
+        self.religion = Skills.Religion()
         # Wisdom dependants
-        self.animal_handling = Skills.Animal_Handling
-        self.insight = Skills.Insight
-        self.medicine = Skills.Medicine
-        self.perception = Skills.Perception
-        self.survival = Skills.Survival
+        self.animal_handling = Skills.Animal_Handling()
+        self.insight = Skills.Insight()
+        self.medicine = Skills.Medicine()
+        self.perception = Skills.Perception()
+        self.survival = Skills.Survival()
         # Charisma dependants
-        self.deception = Skills.Deception
-        self.intimidation = Skills.Intimidation
-        self.performance = Skills.Performance
-        self.persuasion = Skills.Persuasion
+        self.deception = Skills.Deception()
+        self.intimidation = Skills.Intimidation()
+        self.performance = Skills.Performance()
+        self.persuasion = Skills.Persuasion()
         
         #TODO Incorporate saving throws
         #TODO Incorporates races
