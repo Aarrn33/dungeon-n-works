db98f611342944b9f51068a3248d3c58eaca4cb1
Aarrn33 <76686175+Aarrn33@users.noreply.github.com>
2022-10-17 20:47:24 +0200

    fixed bug that resulted in incorret modifier calculation

--- App/Utilities/score2modifier.py
+++ App/Utilities/score2modifier.py
@@ -1,5 +1,6 @@
 from math import floor
 
+# TODO: Add testcases
 # Takes a score from 1 to 30 and returns a modifier from -5 to +10
 def score_2_modifier(score):
-    return floor(score-10)
+    return floor((score-10)/2)
