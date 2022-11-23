60fc4045fce3e6c42044d20d37b31393ddba3bba
Aarrn33 <76686175+Aarrn33@users.noreply.github.com>
2022-11-23 21:49:22 +0100

    Cleaned up the dice utility

--- App/Utilities/dice.py
+++ App/Utilities/dice.py
@@ -1,18 +1,20 @@
 import random
 
+
 class Dice:
-    def __init__(self, size):
+    def __init__(self, size: int) -> None:
         self.size = size
     
     # A function that is used to roll dices
     # For instance a 3d2+5 would be writtent roll(2, nb=3, modifier=5)
-    def roll(self, nb=1, modifier=0):
+    def roll(self, nb: int = 1, modifier: int = 0) -> int:
         res = []
         for i in range(nb):
             res.append(random.randint(1, self.size))
         return sum(res)+modifier
     
 
+# Defines commonly used dices for easier calling
 d4 = Dice(4)
 d6 = Dice(6)
 d8 = Dice(8)
