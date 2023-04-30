a283f58fd07b08ac8bfab7eb0e20cc80c4a5a0cb
Arnaud le Masne de Chermont <arnaudlmdc@gmail.com>
2023-04-30 18:06:13 +0200

    added weight in units added several characteristics to bojects

--- App/Utilities/units.py
+++ App/Utilities/units.py
@@ -1,6 +1,6 @@
 # A class for all units used
 class Unit:
-    def __init__(self, value: float, unit: str) -> None:
+    def __init__(self, value: float, unit: str):
         self.unit = unit
         self.value = value
 
@@ -8,24 +8,37 @@ class Unit:
 # A class used to create and work with distances
 # For instance, it is used to calculate movement speed
 class Distance(Unit):
-    def __init__(self, value: float, unit: str) -> None:
+    def __init__(self, value: float, unit: str):
         assert unit in [
             "m", "ft"], "The unit is not in meters (m) nor in feets (ft) \n Those are the only implemented units for now"
         # The two ifs are used to calculate the value in feet and in meters
         # We round to a maximum of 3 digits after the decimal part
         if unit == "m":
             self.meters = value
-            self.feet = round(self.meter/3.28, 3)
+            self.feet = round(self.meters/3.28, 3)
         elif unit == "ft":
             self.feet = value
             self.meters = round(self.feet*3.28, 3)
         super().__init__(value, unit)
-        self.unit = self.unit
 
 # A class used to work with angles (in degrees)
 
 
 class Angle(Unit):
-    def __init__(self, value: float) -> None:
-        super().__init__(value, "°")
-        self.value = self.value
+    def __init__(self, value: float, unit: str):
+        assert unit in [
+            "°"], "The unit is not in degrees (°) \n This is the only implemented unit for now"
+        super().__init__(value, unit)
+
+
+class Weight(Unit):
+    def __init__(self, value: float, unit: str):
+        assert unit in [
+            "kg", "lb"], "The unit is not in kilograms (kg) nor in pounds (lb) \n Those are the only implemented units for now"
+        if unit == "kg":
+            self.kilograms = value
+            self.pounds = round(self.kilograms*2.21, 3)
+        elif unit == "ft":
+            self.pounds = value
+            self.kilograms = round(self.pounds/2.21, 3)
+        super().__init__(value, unit)
--- App/items.py
+++ App/items.py
@@ -1,22 +1,25 @@
 import App.Utilities.dice as Dices
 import App.Utilities.units as Units
+import App.Utilities.money as Money
 
 # TODO Add weight quantity and a description for items
 # A super class for items
 
 
 class Item:
-    def __init__(self, name: str):
+    def __init__(self, name: str, weight: int, cp_cost: int, description: str = ""):
         self.name = name
+        self.weight = weight
+        self.cost = Money.CP(cp_cost)
+        self.description = description
 
 # A class for stackable items (ie. torches)
 
 
 class Stackable(Item):
-    def __init__(self, name: str, quantity: int = 1):
-        self.name = name
+    def __init__(self, name: str, weight: int, cp_cost: int, quantity: int = 1, description: str = ""):
+        super().__init__(name, weight, cp_cost, description)
         self.quantity = quantity
-        super().__init__(self.name)
         # TODO Add something to merge similar items on item creation
 
 # TODO Add class for measurable items (ie. ropes)
@@ -27,42 +30,31 @@ class Stackable(Item):
 
 
 class Unique(Item):
-    def __init__(self, name: str) -> None:
-        self.name = name
-        super().__init__(self.name)
+    def __init__(self, name: str, weight: int, cp_cost: int, description: str = ""):
+        super().__init__(name, weight, cp_cost, description)
 
 # Adds a class for weapons (ranged and melee)
 
 
 class Weapon(Unique):
-    def __init__(self, name: str, damage: Dices.Dice, saved_dmg: Dices.Dice):
-        self.name = name
+    def __init__(self, name: str, damage: Dices.Dice, weight: int, cp_cost: int, description: str = ""):
         self.damage = damage
-        self.saved_dmg = saved_dmg
-        super().__init__(name)
-        self.uuid = self.uuid
+        super().__init__(name, weight, cp_cost, description)
         # TODO Add a system to incorporate damage type (ie. piercing)
 
 # Adds a class for melee weapons()
 
 
 class Melee(Weapon):
-    def __init__(self, name: str, damage: Dices.Dice):
-        self.name = name
-        self.damage = damage
-        super().__init__(self.name, self.damage)
-        self.uuid = self.uuid
+    def __init__(self, name: str, damage: Dices.Dice, weight: int, cp_cost: int, description: str = ""):
+        super().__init__(name, damage, weight, cp_cost, description)
 
 # Add a class for ranged weapons
 
 
 class Range(Weapon):
-    def __init__(self, name: str, damage: Dices.Dice, saved_dmg: Dices.Dice, n_distance: Units.Distance, l_distance: Units.Distance):
-        self.name = name
-        self.damage = damage
-        self.saved_dmg = saved_dmg
-        super().__init__(name, damage)
-        self.uuid = self.uuid
+    def __init__(self, name: str, damage: Dices.Dice, n_distance: Units.Distance, l_distance: Units.Distance, weight: int, cp_cost: int, description: str = ""):
+        super().__init__(name, damage, weight, cp_cost, description)
         assert n_distance <= l_distance, "The normal range distance must shoreter then the long distance"
         self.n_distance = n_distance
         self.l_distance = l_distance
