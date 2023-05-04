9802efcc62d1cec2fab0b4cf455e7ff4cddf1d90
Arnaud le Masne de Chermont <arnaudlmdc@gmail.com>
2023-05-04 21:26:46 +0200

    added more items added a way to find items

--- App/items.py
+++ App/items.py
@@ -2,7 +2,7 @@ import App.Utilities.dice as Dices
 import App.Utilities.units as Units
 import sqlite3
 
-# TODO Add weight quantity and a description for items
+# TODO Updates the db to include item categories (stackable, equipment, weapon...)
 # A super class for items
 
 
@@ -13,12 +13,19 @@ class Item:
         self.cp_cost = cp_cost
         self.description = description
 
-        # Creates database framework
-        self.conn = sqlite3.connect(r'App\\Objects\\objects.db')
-        self.cursor = self.conn.cursor()
 
-        self.saved_data = [self.name, self.weight,
-                           self.cp_cost, self.description]
+def find(item_name):
+    # Connects to database framework
+    conn = sqlite3.connect(r'App\\Objects\\objects.db')
+    cursor = conn.cursor()
+    cursor.execute(
+        f"""SELECT * FROM objects WHERE name LIKE "%{item_name}%" """)
+    # TODO Update the find function to return a different item class depending on the item's categories
+    data = cursor.fetchall()
+    assert len(data) == 1, f"""Your query return more than one items or no 
+    items at all, it returned {data}"""
+    name, weight, cp_cost, description = data[0]
+    return Item(name, Units.Weight(weight, "lb"), cp_cost, description)
 
 # A class for stackable items (ie. torches)
 
